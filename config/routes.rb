2dcbf9d427f4a83b22af0d5772e07d8481539585
coacer <coacer728h23@icloud.com>
2019-04-09 18:35:34 +0900

    Refactoring user info and book form templates

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :users, only: [:index, :show, :edit, :update]
+
+  resources :books, only: [:index, :show, :edit, :create, :update, :destroy]
 end
