ea75bd84ccc38d3cd8ee57bbe200190292c8d292
coacer <coacer728h23@icloud.com>
2019-04-12 15:20:42 +0900

    Add flash message to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
+      flash[:success] = "User infomation was successfully updated."
       redirect_to @user
     else
       render :edit
