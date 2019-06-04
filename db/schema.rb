226c55527e7562b790a4c8bbc16300157ed5750d
coacer <coacer728h23@icloud.com>
2019-06-04 20:47:46 +0900

    Favoriteモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_02_123949) do
+ActiveRecord::Schema.define(version: 2019_06_04_110755) do
 
   create_table "book_comments", force: :cascade do |t|
     t.string "comment"
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 2019_06_02_123949) do
     t.datetime "updated_at", null: false
     t.text "body"
     t.index ["user_id"], name: "index_books_on_user_id"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_favorites_on_book_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
