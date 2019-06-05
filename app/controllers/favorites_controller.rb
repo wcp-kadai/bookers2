7fe0ff6db6d09a819ac04835444ccb421b405b2b
coacer <coacer728h23@icloud.com>
2019-06-05 19:47:50 +0900

    いいね機能の完成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,9 +1,15 @@
 class FavoritesController < ApplicationController
   def create
-
+    book = Book.find(params[:book_id])
+    favorite = book.favorites.new(user_id: current_user.id)
+    favorite.save
+    redirect_to book_path(book)
   end
 
   def destroy
-
+    book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: book.id)
+    favorite.destroy
+    redirect_to book_path(book)
   end
 end
