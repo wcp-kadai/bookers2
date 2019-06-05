c5abd321a7be87a9b7cda32eac70db7361559b9f
coacer <coacer728h23@icloud.com>
2019-06-05 20:04:57 +0900

    他人のコメントの編集、削除のURLに飛ぶとbookのshowページにリダイレクトされる機能実装

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,4 +1,7 @@
 class BookCommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :correct_user, only: [:edit, :update, :destroy]
+
   def create
     @book = Book.find(params[:book_id])
     @book_new = Book.new
@@ -40,5 +43,13 @@ class BookCommentsController < ApplicationController
 
   def book_comment_params
     params.require(:book_comment).permit(:comment)
+  end
+
+  def correct_user
+    book_comment = BookComment.find(params[:id])
+    if current_user != book_comment.user
+      redirect_to book_path(book_comment.book)
+    end
+
   end
 end
