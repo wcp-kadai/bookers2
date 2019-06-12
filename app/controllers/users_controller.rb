bc03f4ab45b201a8eb1aa02fd3ad795208a769f9
coacer <coacer728h23@icloud.com>
2019-06-12 21:10:01 +0900

    follows/followersページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,16 @@ class UsersController < ApplicationController
     end
   end
 
+  def follows
+    user = User.find(params[:id])
+    @users = user.followings
+  end
+
+  def followers
+    user = User.find(params[:id])
+    @users = user.followers
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
