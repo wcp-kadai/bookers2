35bc7cc272eb2f03d2181eca959bc4a7ab136257
coacer <coacer728h23@icloud.com>
2019-06-12 20:42:37 +0900

    follow機能完成

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,15 @@
 class RelationshipsController < ApplicationController
+  def create
+    user = User.find(params[:user_id])
+    follow = current_user.active_relationships.build(follower_id: user.id)
+    follow.save
+    redirect_to user_path(user)
+  end
+
+  def destroy
+    user = User.find(params[:user_id])
+    follow = current_user.active_relationships.find_by(follower_id: user.id) 
+    follow.destroy
+    redirect_to user_path(user)
+  end
 end
