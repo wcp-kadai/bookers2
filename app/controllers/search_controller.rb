3066c15ebd3a2fa6b45c731580268a5a01f3ed43
coacer <coacer728h23@icloud.com>
2019-11-16 14:04:08 +0900

    検索機能を完全一致のみに変更

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,54 +1,18 @@
 class SearchController < ApplicationController
+  before_action :authenticate_user!
+
   def search
-    @model = params["search"]["model"] 
+    @model = params["search"]["model"]
     @content = params["search"]["content"]
-    @how = params["search"]["how"]
-    @datas = search_for(@how, @model, @content) 
+    @records = search_for(@model, @content)
   end
 
   private
-  def perfect_match(model, content)
+  def search_for(model, content)
     if model == 'user'
       User.where(name: content)
     elsif model == 'book'
       Book.where(title: content)
-    end
-  end
-
-  def forward_match(model, content)
-    if model == 'user'
-      User.where("name like '#{content}%'")
-    elsif model == 'book'
-      Book.where("title like '#{content}%'")
-    end
-  end
-
-  def backward_match(model, content)
-    if model == 'user'
-      User.where("name like '%#{content}'")
-    elsif model == 'book'
-      Book.where("title like '%#{content}'")
-    end
-  end
-
-  def partial_match(model, content)
-    if model == 'user'
-      User.where("name like '%#{content}%'")
-    elsif model == 'book'
-      Book.where("title like '%#{content}%'")
-    end
-  end
-
-  def search_for(how, model, content)
-    case how
-    when 'perfect'
-      perfect_match(model, content)
-    when 'forward'
-      forward_match(model, content)
-    when 'backward'
-      backward_match(model, content)
-    when 'partial'
-      partial_match(model, content)
     end
   end
 
