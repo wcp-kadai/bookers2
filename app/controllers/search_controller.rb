56f96c0782ffe8eabf77df85bc30ec07ee08a8aa
coacer <coacer728h23@icloud.com>
2019-12-14 15:28:44 +0900

    fix + コメント追加

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,18 +1,18 @@
 class SearchController < ApplicationController
   before_action :authenticate_user!
 
-  def search
+  def index
     @model = params["search"]["model"]
     @content = params["search"]["content"]
     @records = search_for(@model, @content)
   end
 
   private
-  def search_for(model, content)
-    if model == 'user'
-      User.where(name: content)
-    elsif model == 'book'
-      Book.where(title: content)
+  def search_for(model, content) # モデルと検索ワードによって検索するメソッド
+    if model == 'user' # 対象モデルがUserの場合
+      User.where("name like ?", "%#{content}%")
+    elsif model == 'book' # 対象モデルがBookの場合
+      Book.where("title like ?", "%#{content}%")
     end
   end
 
