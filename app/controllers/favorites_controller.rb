f3d743fb0cded7b8137a560c86ea2e1fe1d75dd2
coacer <coacer728h23@icloud.com>
2019-12-14 14:40:32 +0900

    コメント追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,22 +3,22 @@ class FavoritesController < ApplicationController
     @book = Book.find(params[:book_id])
     favorite = @book.favorites.new(user_id: current_user.id)
     favorite.save
-    redirect
+    redirect # 下で定義したメソッドを呼び出し
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect
+    redirect # 下で定義したメソッドを呼び出し
   end
 
   private
   def redirect
     case params[:redirect_id].to_i
-    when 0
+    when 0 # books/indexページからいいねした場合
       redirect_to books_path
-    when 1
+    when 1 # books/showページからいいねした場合
       redirect_to book_path(@book)
     end
   end
