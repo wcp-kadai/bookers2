c1c58cf8ce52a10d4364000e31023160eec139e1
coacer <coacer728h23@icloud.com>
2019-12-15 15:59:17 +0900

    非同期フォロー機能

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,15 +1,19 @@
 class RelationshipsController < ApplicationController
   def create
-    user = User.find(params[:user_id])
-    follow = current_user.active_relationships.build(follower_id: user.id)
+    @user = User.find(params[:user_id])
+    @render_flag = params[:render_flag].to_i
+    follow = current_user.active_relationships.build(follower_id: @user.id)
     follow.save
-    redirect_to user_path(user)
+    # redirect_to user_path(user)
+    render 'users/relation_change'
   end
 
   def destroy
-    user = User.find(params[:user_id])
-    follow = current_user.active_relationships.find_by(follower_id: user.id) 
+    @user = User.find(params[:user_id])
+    @render_flag = params[:render_flag].to_i
+    follow = current_user.active_relationships.find_by(follower_id: @user.id)
     follow.destroy
-    redirect_to user_path(user)
+    # redirect_to user_path(user)
+    render 'users/relation_change'
   end
 end
